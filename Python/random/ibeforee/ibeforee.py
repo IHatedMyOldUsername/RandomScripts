d49962c6983d8a8a1a221c47e789885a631d7d1b
Jonah Nichols <nicholskids.jonah@gmail.com>
2020-01-15 21:05:35 -0800

    Changed testing on ibeforee.py

--- Python/random/ibeforee/ibeforee.py
+++ Python/random/ibeforee/ibeforee.py
@@ -1,26 +1,27 @@
-import re;
+import re
 
 win=0
 fail = 0
 with open('words.txt') as words:
     wordList = words.readlines()
     for word in wordList:
-        for i in range(0,len(word)+1):
-            testStr = word[i:i+3]
-            print(testStr)
-            if(testStr == 'cie'):
-                fail = fail+1
-                break
-            elif(testStr == 'cei'):
-                win = win+1
-                break
-            elif(testStr[:1] != 'c'):
-                if(testStr[1:2] == 'ei'):
-                    fail = fail+1
-                    break
-                elif(testStr[1:2] == 'ie'):
-                    win = win+1
-                    break
+        print(word)
+        if('cie' in word):
+            fail = fail+1
+            print(fail)
+            continue
+        elif('cei' in word):
+            win = win+1
+            print(win)
+            continue
+        elif(re.search('.ei', word)):
+            fail = fail+1
+            print(fail)
+            continue
+        elif(re.search('.ie', word)):
+            win = win+1
+            print(win)
+            continue
     
     print("Number of words following rule: " + str(win))
     print("Number of words not following rule: " + str(fail))
