2c4f5503706ecffc05fc94333cb8c66da15ed762
Jonah Nichols <nicholskids.jonah@gmail.com>
2020-01-15 20:40:32 -0800

    Finished rough ibeforee

--- Python/random/ibeforee/ibeforee.py
+++ Python/random/ibeforee/ibeforee.py
@@ -1,10 +1,28 @@
 import re;
 
-def mainfunct(word):
-   print('hi') ;
-    
-
+win=0
+fail = 0
 with open('words.txt') as words:
-    wordList = words.readlines();
+    wordList = words.readlines()
     for word in wordList:
-        print('high')
+        for i in range(0,len(word)+1):
+            testStr = word[i:i+3]
+            print(testStr)
+            if(testStr == 'cie'):
+                fail = fail+1
+                break
+            elif(testStr == 'cei'):
+                win = win+1
+                break
+            elif(testStr[:1] != 'c'):
+                if(testStr[1:2] == 'ei'):
+                    fail = fail+1
+                    break
+                elif(testStr[1:2] == 'ie'):
+                    win = win+1
+                    break
+    
+    print("Number of words following rule: " + str(win))
+    print("Number of words not following rule: " + str(fail))
+
+    print(fail/(win+fail))
