83e0cb7ebf0900fee9d55be233018419ebf233a8
Jonah Nichols <nicholskids.jonah@gmail.com>
2020-05-20 02:21:33 -0700

    Continued loontune.py, added stuff

--- Python/random/loontune.py
+++ Python/random/loontune.py
@@ -1,3 +1,21 @@
-import requests
+import urllib.request
+from bs4 import BeautifulSoup
+import os
 
-apiLink = 'api.thetvdb.com'
+episodeList = ['01 - Horton Hatches the Egg.avi',  '10 - Much Ado About Nutting.avi',
+'02 - Lights Fantastic.avi',        '11 - The Hole Idea.avi',
+'03 - Fresh Airedale.avi',          '12 - Now Hear This.avi',
+'04 - Chow Hound.avi',              '13 - Martian Through Georgia.avi',
+'05 - The Oily American.avi',       '14 - Page Miss Glory.avi',
+"06 - It's Hummer Time.avi",        '15 - Norman Normal.avi']
+for i in range(1,27):
+    loontunalt = urllib.request.urlopen('https://www.thetvdb.com/series/looney-tunes/seasons/dvd/' + str(i))
+    soup = BeautifulSoup(loontunalt, features="lxml")
+    for tabrow in soup.find_all('tr'):
+        for ep in tabrow.find_all('a'):
+            epDB = ep.contents[0].strip()
+            for realEp in episodeList:
+                if(realEp[5:][:-4] == epDB):
+                    SeasonEp = str(tabrow.find('td')).strip()[4:-5]
+
+#To remind yourself: you were working on getting season episode data out of what you just did
